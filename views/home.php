9d96f74bc7e2d1fc13a6a078b534a089769a4453
Андрей Харьков <146940010+smuutta@users.noreply.github.com>
2025-06-28 04:20:31 +0300

    Update home.php

--- views/home.php
+++ views/home.php
@@ -9,7 +9,7 @@ include __DIR__ . '/../partials/header.php';
     <li><a href="index.php?route=sale/form">Продажа товара (чек)</a></li>
     <li><a href="index.php?route=report">Отчёт по продажам</a></li>
     <li><a href="index.php?route=product/form">Управление товарами</a></li>
-    <li><a href="index.php?route=employee/form">Управление товарами</a></li>
+    <li><a href="index.php?route=employee/form">Управление сотрудниками</a></li>
 </ul>
 <?php
 include __DIR__ . '/../partials/footer.php';
